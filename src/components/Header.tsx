4b6ee345f0045a253461209efe4cfa1d7ba54262
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-04 21:44:16 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
                     name="reorder-two-outline" 
                     size="large"
                 >
-                </IonIcon> {/* Replace 'ion-icon' with  */}
+                </IonIcon> {/* Replace 'ion-icon' */}
             </div>
         </div>
     )
