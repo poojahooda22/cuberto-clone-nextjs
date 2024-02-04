5d095d082145c286e0452c02f971b36c51c532c3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-04 21:46:02 +0530

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ function Header() {
                     name="reorder-two-outline" 
                     size="large"
                 >
-                </IonIcon> {/* Repla */}
+                </IonIcon> {/* Repl */}
             </div>
         </div>
     )
