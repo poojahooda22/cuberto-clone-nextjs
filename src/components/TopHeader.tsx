09c6771ec62368fd38d44a1442aaa948efc26c8e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-05 20:17:44 +0530

    updated topheader

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './TopHeader.css';
 
+
 function TopHeader() {
   return (
       <section className='mx-[4rem] my-[32rem] mb-28 sm:mx-[8rem] sm:my-[8rem] lg:mx-[24rem] lg:my-[8rem]
@@ -8,18 +9,18 @@ function TopHeader() {
         <div className='flex flex-col justify-center'>
             <h1 className=''>
                 <div className=' relative text-2xl font-light inline-block mr-6 md:text-xl leading-3'>We</div>
-                <em><div className='tophead-title-em text-xl inline-block font-thin'>develop</div></em>
+                <em><div className='tophead-title-em  inline-block font-light '>develop</div></em>
                 <div className='flex flex-row items-center '>
                     <span className='tophead-title-span'>
                         <video width="100%" height="100%" preload="auto" autoPlay playsInline loop muted 
                             src="/videos/1.mp4" className='tophead-title-video '
                         ></video>  
-                        <div className='inline-block text-2xl mr-[2rem]'>digital </div>  
+                        <div className='inline-block text-2xl mr-[2rem] font-light'>digital </div>  
                     </span>
-                    <div className='text-2xl'> products</div>
+                    <div className='text-2xl font-light'> products</div>
                 </div>
                 <div className='flex flex-row'>
-                    <div className='text-2xl mr-[2rem]'>from</div>
+                    <div className='text-2xl mr-[2rem] font-light'>from</div>
                     <div className='text-2xl'>scratch</div>
                 </div>
                 
