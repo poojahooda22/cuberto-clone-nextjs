3ad68b9e9fd67f3de4d196c04c129dd1967cb4e1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-05 22:03:07 +0530

    updated top header

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -4,14 +4,15 @@ import './TopHeader.css';
 
 function TopHeader() {
   return (
-      <section className='mx-[4rem] my-[12rem] mb-28 sm:mx-[8rem] sm:my-[8rem] lg:mx-[24rem] lg:my-[8rem]
-      sm:mb-0 scroll-mt-[100rem] '>
+      <section className='mx-[2rem] my-[8rem] mb-28 sm:mx-[8rem] sm:my-[8rem] lg:mx-[24rem] lg:my-[8rem]
+      sm:mb-0 scroll-mt-[100rem] h-[100%]'>
         <div className='flex flex-col justify-center overflow-hidden '>
             <div className=''>
                 <h1>
-                    <div>
-                        <div>We</div>
+                    <div className='relative -m-[0.15rem] p-[0.15rem] inline-block'>
+                        <div className='text-md'>We</div>
                     </div>
+                    <em></em>
                 </h1>
             </div>
             {/* <h1 className=''>
