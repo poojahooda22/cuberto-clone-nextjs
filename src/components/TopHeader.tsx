6fa50ae2c06a1aed649b87f61e2460ab9f81cfca
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-05 21:48:12 +0530

    updated

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -4,16 +4,25 @@ import './TopHeader.css';
 
 function TopHeader() {
   return (
-      <section className='mx-[4rem] my-[32rem] mb-28 sm:mx-[8rem] sm:my-[8rem] lg:mx-[24rem] lg:my-[8rem]
-      sm:mb-0 scroll-mt-[100rem]'>
-        <div className='flex flex-col justify-center'>
-            <h1 className=''>
-                <div className=' relative text-2xl font-light inline-block mr-6 md:text-xl leading-3'>We</div>
+      <section className='mx-[4rem] my-[12rem] mb-28 sm:mx-[8rem] sm:my-[8rem] lg:mx-[24rem] lg:my-[8rem]
+      sm:mb-0 scroll-mt-[100rem] '>
+        <div className='flex flex-col justify-center overflow-hidden '>
+            <div className=''>
+                <h1>
+                    <div>
+                        <div>We</div>
+                    </div>
+                </h1>
+            </div>
+            {/* <h1 className=''>
+                <div className='relative text-2xl inline-block mr-6'>
+                    We
+                </div>
                 <em><div className='tophead-title-em  inline-block font-light '>develop</div></em>
                 <div className='flex flex-row items-center '>
                     <span className='tophead-title-span'>
                         <video width="100%" height="100%" preload="auto" autoPlay playsInline loop muted 
-                            src="/videos/1.mp4" className='tophead-title-video '
+                            src="/videos/1.mp4" className='w-[12.2rem] h-[7.6rem] object-cover rounded-[1000px]'
                         ></video>  
                         <div className='inline-block text-2xl mr-[2rem] font-light'>digital </div>  
                     </span>
@@ -22,9 +31,8 @@ function TopHeader() {
                 <div className='flex flex-row'>
                     <div className='text-2xl mr-[2rem] font-light'>from</div>
                     <div className='text-2xl'>scratch</div>
-                </div>
-                
-            </h1>
+                </div>            
+            </h1> */}
             
         </div> 
       </section>
