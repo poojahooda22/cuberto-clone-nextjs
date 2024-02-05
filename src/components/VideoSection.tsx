214ac4d8abd3f0e3e1df4f785f6bd1a9ff52b232
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-06 00:01:36 +0530

    created video section

---
+++ src/components/VideoSection.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function VideoSection() {
+  return (
+    <section>
+        <div>
+            <video 
+                width="100%" 
+                height="100%" 
+                preload="auto" 
+                autoPlay 
+                playsInline 
+                loop 
+                muted 
+                src="/videos/1.mp4" className='w-[100%] h-[100%] object-cover '
+            ></video>
+        </div>
+      
+    </section>
+  )
+}
+
+export default VideoSection
