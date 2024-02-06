c6c9916309aada82107f79f22d6ea1a3dba33c70
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-06 19:56:03 +0530

    updated

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -17,8 +17,7 @@ function TopHeader() {
                         className='relative -m-[9.15px] p-[9.15px] inline-block'
                     >
                         <div className='mr-2'>
-                            We
-</div>
+                            We</div>
                     </div>
                     <em>
                         <div className='relative -m-[9.15px] p-[9.15px] inline-block'>
