5c0f2c6f41c45846959d6235caefbad330a8e862
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-06 10:47:27 +0530

    updated

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -35,8 +35,7 @@ function TopHeader() {
                             <div>
                                 <video width="100%" height="100%" preload="auto" autoPlay playsInline loop muted 
                                     src="/videos/1.mp4" 
-                                    className='w-[5.4rem] h-[3.6rem] object-cover rounded-[1000px] mr-[12px] 
-                                    
+                                    className='w-[5.4rem] h-[3.6rem] object-cover rounded-[1000px] mr-[12px]                                    
                                     lg:w-[167px] lg:h-[115px]
                                     xl:w-[223px] xl:h-[153px]'
                                 ></video>
