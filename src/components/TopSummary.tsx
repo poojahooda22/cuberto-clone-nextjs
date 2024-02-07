b30ae859ef1b1ee21d7d4d7142e1b17f2b052fd1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-07 11:31:13 +0530

    updated top summary

--- src/components/TopSummary.tsx
+++ src/components/TopSummary.tsx
@@ -6,22 +6,24 @@ function TopSummary() {
       <div className='mx-[320px]'>
         <div className='md:flex gap-4'>
             <div>
-                <video 
-                    width="100%" 
-                    height="100%" preload="auto" 
-                    autoPlay 
-                    playsInline loop muted 
-                                src="/videos/1.mp4" 
-                                    className='w-[5.4rem] h-[3.6rem] object-cover rounded-[1000px] mr-[12px]                                    
-                                    lg:w-[167px] lg:h-[115px]
-                                    xl:w-[223px] xl:h-[153px]'
-                                ></video>
-            </div>
-            <div>
                 <div>
-                    <h3>Cuberto is a leading digital product agency focused on branding, UI/UX design, mobile, and web development.</h3>
+                    <video 
+                        width="100%" 
+                        height="100%" preload="auto" 
+                        autoPlay 
+                        playsInline loop muted 
+                                src="/videos/1.mp4" 
+                                        className='w-[5.4rem] h-[3.6rem] object-cover rounded-[1000px] mr-[12px]                                    
+                                        lg:w-[167px] lg:h-[115px]
+                                        xl:w-[223px] xl:h-[153px]'
+                                    ></video>
                 </div>
-                <button>How we Work</button>
+                <div>
+                    <div>
+                        <p>Cuberto is a leading digital product agency focused on branding, UI/UX design, mobile, and web development.</p>
+                    </div>
+                    <button>How we Work</button>
+                </div>
             </div>
         </div>
       </div>
