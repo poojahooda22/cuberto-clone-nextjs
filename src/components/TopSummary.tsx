0d15bca3cbb808a4d59a92abf1486821360c9c01
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-07 21:31:12 +0530

    updated summary

--- src/components/TopSummary.tsx
+++ src/components/TopSummary.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TopSummary() {
   return (
-    <section className=' pt-[67px] pb-[90px]'>
+    <section className=' pt-[67px] pb-[90px] w-full '>
       <div className=' px-[16px] md:px-[81px] '>
         <div className=''>
             <div className='md:flex justify-center items-center'>
@@ -12,7 +12,7 @@ function TopSummary() {
                         height="100%" preload="auto" 
                         autoPlay 
                         playsInline loop muted 
-                                src="/videos/summary.mp4" 
+                            src="/videos/summary.mp4" 
                                         className=' object-cover rounded-[1000px] mb-[56px] 
                                         w-[361px] h-[361px]                                   
                                         '
