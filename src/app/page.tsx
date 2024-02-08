0cdd9668533009062808bee50d04d98342eea668
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-08 16:29:32 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <TopHeader  />
       <VideoSection />
       <TopSummary />
