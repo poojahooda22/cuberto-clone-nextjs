1ecd4d8abe0d403160dd2341781d24133845daa1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 13:17:29 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 import { useRef } from 'react';
 import Image from "next/image";
-import { useEffect  } from "react";
+
 
 
 import 'locomotive-scroll/dist/locomotive-scroll.css';
