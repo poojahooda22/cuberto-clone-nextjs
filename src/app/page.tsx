ea80f05a5b0dce050ce82b13edaada32c2325e39
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 12:40:21 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
 import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
+import { useRef } from 'react';
 import Image from "next/image";
 import { useEffect  } from "react";
 import { LocomotiveScrollProvider as RLSProvider } from 'react-locomotive-scroll';
@@ -13,7 +14,8 @@ import 'locomotive-scroll/dist/locomotive-scroll.css';
 
 
 export default function Home({ Component, pageProps }: AppProps) {
-
+  const { asPath } = useRouter();
+  const containerRef = useRef(null);
   useEffect(() => {
     
   })
