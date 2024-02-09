d3cd56b37c74dbb2ca8eadedfc7e6670333dbcba
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 13:18:17 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,6 @@ import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 
 
-
-
-
-import 'locomotive-scroll/dist/locomotive-scroll.css';
-
-
 export default function Home({ Component, pageProps }: AppProps) {
  
   return (
