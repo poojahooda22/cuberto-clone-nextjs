5a062d56bcb86dc956dce2fe6e3593bf12747c08
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 12:34:14 +0530

    updated script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,15 @@ import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
 import Image from "next/image";
 import { useEffect  } from "react";
+import { LocomotiveScrollProvider as RLSProvider } from 'react-locomotive-scroll';
+
+import 'locomotive-scroll/dist/locomotive-scroll.css';
+import '../styles/globals.css';
 
 export default function Home() {
 
   useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import('locomotive-scroll')).default;
-      const locomotiveScroll = new LocomotiveScroll();
-    })()
+    
   })
   return (
     <main>
