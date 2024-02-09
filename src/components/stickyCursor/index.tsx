325c5483d0bcb72f90a6c936fa63b7fe45fd1909
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 16:12:58 +0530

    updated

--- src/components/stickyCursor/index.tsx
+++ src/components/stickyCursor/index.tsx
@@ -1,24 +1,32 @@
-import React, {useEffect} from 'react'
+
+import React, { useEffect } from 'react'
 import styles from './style.module.scss';
 import {motion, useMotionValue} from 'framer-motion';
 
-function index() {
-
+export default function Index() {
+  const cursorSize = 10;
   const mouse = {
     x: useMotionValue(0),
     y: useMotionValue(0)
   }
-
-  const manageMouseMove = (e) => {
-    const { clientX, clientY} =e;
-    mouse.x.set(clientX) 
-    mouse.y.set(clientY)
+  const manageMouseMove = (e: any) => {
+    const { clientX, clientY} = e;
+    mouse.x.set(clientX - cursorSize / 2) 
+    mouse.y.set(clientY - cursorSize / 2)
   }
+
+  useEffect( () => {
+    window.addEventListener("mousemove", manageMouseMove)
+    return () => {window.removeEventListener("mousemove", manageMouseMove)}
+  })
+
   return (
-    <motion.div className={styles.cursor}>
-      
+    <motion.div 
+    className={styles.cursor}
+    style={{left: mouse.x, top: mouse.y}}
+    >
+    
     </motion.div>
   )
 }
 
-export default index
