a5691e8a3c32854e47aa096fff96c8c10e5d87a4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 13:50:23 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import Header from "@/components/Header";
+import SmoothScroll from "@/components/SmoothScroll";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="text-slate-900">
-        <Header />
-        {children}
+        <SmoothScroll>
+          <Header />
+          {children}
+        </SmoothScroll>
+        
+        
       </body>
     </html>
   );
