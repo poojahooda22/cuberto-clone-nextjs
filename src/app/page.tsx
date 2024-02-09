3dffeacad3001e40ea778176acdf4cad7225e584
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 12:36:18 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@
 import TopHeader from "@/components/TopHeader";
 import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
+import { useRouter } from 'next/router';
+import type { AppProps } from 'next/app';
 import Image from "next/image";
 import { useEffect  } from "react";
 import { LocomotiveScrollProvider as RLSProvider } from 'react-locomotive-scroll';
 
 import 'locomotive-scroll/dist/locomotive-scroll.css';
-import '../styles/globals.css';
 
-export default function Home() {
+
+export default function Home({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     
