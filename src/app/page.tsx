b595434eb0f74a16b58b845bc42ff05408ab6c9c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 12:15:24 +0530

    updated scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   useEffect(() => {
     (async () => {
-      
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
     })
   })
   return (
