6be4449d9f7dc2533f0a37b8b8ec99bb587bd7bd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 13:18:27 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 
 
-export default function Home({ Component, pageProps }: AppProps) {
+export default function Home() {
  
   return (
       <main>
