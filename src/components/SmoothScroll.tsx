5cceae2b749a277bf341bb43db06a8595a9a84e3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 13:39:08 +0530

    updated scroll

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -3,10 +3,15 @@
 import React from 'react'
 import { ReactLenis } from '@studio-freight/react-lenis';
 
-function SmoothScroll() {
+
+function SmoothScroll({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
   return (
     <ReactLenis root>
-      
+        {children}
     </ReactLenis>
   )
 }
