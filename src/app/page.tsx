ff6012c6102a26892b8c98e6414d71e10c9739f2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 15:27:41 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+'use client';
 
 import TopHeader from "@/components/TopHeader";
 import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
-
+import Cursor from "@/components/stickyCursor";
 
 export default function Home() {
  
   return (
       <main>
+        <Cursor />
         <TopHeader  />
         <VideoSection />
         <TopSummary />
