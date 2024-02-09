03cb3c6e0a1cd8c10ef078e6504c54ae61a198bc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 13:24:09 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-'use client';
 
 import TopHeader from "@/components/TopHeader";
 import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
-import { useRouter } from 'next/router';
-import type { AppProps } from 'next/app';
 
 
 export default function Home() {
