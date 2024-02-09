2215735a793e91a118d6dcb7d0d26f324e24c7fe
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 15:53:00 +0530

    updated

--- src/components/stickyCursor/index.tsx
+++ src/components/stickyCursor/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './style.module.scss';
 import {motion, useMotionValue} from 'framer-motion';
 
@@ -7,6 +7,12 @@ function index() {
   const mouse = {
     x: useMotionValue(0),
     y: useMotionValue(0)
+  }
+
+  const manageMouseMove = (e) => {
+    const { clientX, clientY} =e;
+    mouse.x.set(clientX) 
+    mouse.y.set(clientY)
   }
   return (
     <motion.div className={styles.cursor}>
