02f6d4e9a9e51698d70a23ca2074ced0c3fe2e80
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-09 14:01:43 +0530

    created index

---
+++ src/components/stickyCursor.tsx/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './stickyCursor.module.css'
+
+function index() {
+  return (
+    <div className={styles.cursor}>
+      
+    </div>
+  )
+}
+
+export default index
