ed68a930796678f7b09661a7c6b0761de9e15605
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-10 19:32:46 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import TopHeader from "@/components/TopHeader";
 import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
 import Cursor from "@/components/stickyCursor/Index";
-import Header from "@/components/Header";
+import Header from "@/components/header/index";
 import { useRef } from 'react';
 
 export default function Home() {
