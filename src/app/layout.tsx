022a21a42ab8170efc2b73ddc7dc9d98311b7ffb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-10 13:08:34 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import Header from "@/components/Header";
+
 import SmoothScroll from "@/components/SmoothScroll";
 
 
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="text-slate-900">
         <SmoothScroll>
-          <Header />
+          
           {children}
         </SmoothScroll> 
       </body>
