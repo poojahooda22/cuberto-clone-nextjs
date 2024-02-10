8828d7b5a6834752635902c63d9d57e00a65b8db
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-10 13:02:06 +0530

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import  IonIcon  from '@reacticons/ionicons'
 import  Image from 'next/image';
 import Link from 'next/link';
+import { forwardRef } from 'react';
+
 
 function Header() {
     return (
