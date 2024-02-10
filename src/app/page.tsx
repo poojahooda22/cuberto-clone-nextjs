0c025386ff4993fd07ea84c6a53f070ec8cde349
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-10 13:08:42 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import TopHeader from "@/components/TopHeader";
 import TopSummary from "@/components/TopSummary";
 import VideoSection from "@/components/VideoSection";
 import Cursor from "@/components/stickyCursor/Index";
+import Header from "@/components/Header";
+import { useRef } from 'react';
 
 export default function Home() {
  
+  const stickyElement = useRef(null);
   return (
       <main>
+        <Header />
         <Cursor />
         <TopHeader  />
         <VideoSection />
