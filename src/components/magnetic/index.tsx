fc92dd10a3721fed38880b0b6282726d7e123ede
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-11 14:31:44 +0530

    updated

--- src/components/magnetic/index.tsx
+++ src/components/magnetic/index.tsx
@@ -6,13 +6,27 @@ export default function Framer({children}: any) {
 
     const [ position, setPosition] = useState({x:0, y:0});
 
+    const handleMouse = (e) => {
+        const { clientX, clientY} = e;
+        const { height, width, left, top} = ref.current.getBoundingClientRect();
+        const middleX = clientX - (left + width/2)
+        const middleY = clientY - (top + height /2)
+        setPosition({x: middleX * 0.1, y: middleY * 0.1})
+    }
+
+    const reset = () => {
+        setPosition({x:0, y:0})
+    }
+
+    const { x, y } = position;
+
     return (
         <motion.div
             style={{position: "relative"}}
             ref={ref}
             onMouseMove={handleMouse}
             onMouseLeave={reset}
-            animate={{x,y}}
+            animate={{x, y}}
             transition={{type: "spring", stiffness: 350, damping: 5, mass: 0.5 }}
         >
             {children}
