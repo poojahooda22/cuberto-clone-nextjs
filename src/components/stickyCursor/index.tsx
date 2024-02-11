1d568e7787480888586ccf7d770dc61ba0f4c5c7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-11 16:00:59 +0530

    updated index

--- src/components/stickyCursor/index.tsx
+++ src/components/stickyCursor/index.tsx
@@ -1,13 +1,15 @@
 
 import React, { useEffect, useState, useRef } from 'react'
 import styles from './style.module.scss';
-import {motion, transform, useMotionValue, useSpring} from 'framer-motion';
+import {motion, transform, useMotionValue, useSpring, animate} from 'framer-motion';
+
 
 
 export default function Index({stickyElement}: any) {
   const [isHovered, setIsHovered] = useState(false);
-  const cursor = useRef(null);
+  const cursorRef = useRef();
   const cursorSize = isHovered ? 60 : 10;
+
   const mouse = {
     x: useMotionValue(0),
     y: useMotionValue(0)
@@ -23,6 +25,10 @@ export default function Index({stickyElement}: any) {
     y: useMotionValue(1)
   }
 
+  const rotate = (distance: any) => {
+    const angle = Math.atan2(distance.y, distance.x)
+  }
+
   const manageMouseMove = (e: any) => {
     const { clientX, clientY} = e;
     const { left, top, width, height } = stickyElement.current.getBoundingClientRect();
@@ -30,22 +36,24 @@ export default function Index({stickyElement}: any) {
     const center = {x: left + width / 2, y: top + height / 2};
     const distance = { x: clientX - center.x, y: clientY - center.y };
 
-
-    
-
     if(isHovered) {
+
+      
+
+      //rotation
+      rotate(distance)
 
       //stretch the cursor based on the distance between the pointer and the custom cursor
       const absDistance = Math.max(Math.abs(distance.x), Math.abs(distance.y));
-      const newScaleX = transform(absDistance, [0, width/2], [1, 1.3])
-      const newScaleY = transform(absDistance, [0, height/2], [1, 0.8])
+      const newScaleX = transform(absDistance, [0, height/2], [1, 1.3])
+      const newScaleY = transform(absDistance, [0, width/2], [1, 0.8])
       scale.x.set(newScaleX);
       scale.y.set(newScaleY);
-
 
       mouse.x.set((center.x - cursorSize / 2) + distance.x * 0.1 )
       mouse.y.set((center.y - cursorSize / 2) + distance.y  * 0.1)
     } else {
+
       mouse.x.set(clientX - cursorSize / 2) 
       mouse.y.set(clientY - cursorSize / 2)
     }
@@ -55,8 +63,9 @@ export default function Index({stickyElement}: any) {
   const manageMouseOver = () => {
     setIsHovered(true);
   }
-  const manageMouseLeave = () => {
+  const manageMouseLeave = e => {
     setIsHovered(false);
+    animate(cursorRef.current, { scaleX: 1, scaleY: 1 }, {duration: 0.1}, {type: "spring"})
   }
 
   useEffect( () => {
@@ -68,10 +77,11 @@ export default function Index({stickyElement}: any) {
       stickyElement.current.removeEventListener("mouseover", manageMouseOver)
       stickyElement.current.removeEventListener("mouseleave", manageMouseLeave)
     }
-  })
+  },)
 
   return (
     <motion.div 
+    ref={cursorRef}
     className={styles.cursor}
     style={{
       left: smoothMouse.x, 
