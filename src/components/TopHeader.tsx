72156b148b322ce5553486dc761f64effcf505ce
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-12 22:12:38 +0530

    updated

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -11,8 +11,7 @@ function TopHeader() {
     >
         <div 
             className='overflow-hidden mx-[1rem] my-[8rem] 
-            mb-28 
-            sm:mx-[4rem] sm:my-[4rem]       
+            mb-28 sm:mx-[4rem] sm:my-[4rem]       
             lg:mx-[220px] xl:px-[272px]'>           
                 <h1  
                     className='text-sm tracking-tighter md:text-lg  
