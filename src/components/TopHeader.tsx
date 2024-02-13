d0d1aa51a0af9cb187e1536cb5a5a90f2b292ba5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-13 20:30:55 +0530

    updated

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -12,7 +12,8 @@ function TopHeader() {
         <div 
             className=' flex items-center min-h-[50vh] overflow-hidden mx-[1rem] my-[8rem] 
             mb-28 sm:mx-[4rem] sm:my-[4rem]       
-            lg:mx-[220px] xl:px-[272px]'>           
+            lg:mx-[220px] xl:px-[272px]'
+            >           
                 <h1  
                     className='text-sm tracking-tighter md:text-lg  
                     lg:text-xl xl:text-2xl'>
