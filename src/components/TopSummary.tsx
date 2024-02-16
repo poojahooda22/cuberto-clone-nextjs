d70005b6e380a622afd1ae33c981f8603eeaec6a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-16 20:42:58 +0530

    updated

--- src/components/TopSummary.tsx
+++ src/components/TopSummary.tsx
@@ -41,7 +41,7 @@ function TopSummary() {
                                 )}
                             >
                                 <span className=
-                                    "absolute inset-0 z-0 h-full translate-y-28 bg-[#000] transition-transform duration-700">
+                                    "absolute inset-0 z-0 h-full translate-y-28 bg-[#000] transition-transform ">
                                 </span>                                                                                                                                                                                      
                             </span> */}
 
