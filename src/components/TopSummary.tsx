27f18cd4e602f938cdafdf41689781a18bdbec2c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-17 21:53:00 +0530

    updated

--- src/components/TopSummary.tsx
+++ src/components/TopSummary.tsx
@@ -35,7 +35,7 @@ function TopSummary() {
                     <Link 
                         href='/about'
                     >                  
-                        {/* \*/}
+                        
                         <button className="cb-btn cb-btn_more">
                             <span className="cb-btn_more-title">
                                 <span>How we work</span>
