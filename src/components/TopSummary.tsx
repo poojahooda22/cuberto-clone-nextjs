ae117e711db396aaa38cf6b85c118b364d706f6f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-17 21:49:55 +0530

    updated

--- src/components/TopSummary.tsx
+++ src/components/TopSummary.tsx
@@ -36,7 +36,7 @@ function TopSummary() {
                         href='/about'
                     >                  
                         {/* <span
-                            classN*/}
+                            clas*/}
                         <button className="cb-btn cb-btn_more">
                             <span className="cb-btn_more-title">
                                 <span>How we work</span>
