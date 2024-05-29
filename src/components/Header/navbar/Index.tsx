987fca0ee5c2e8bb6ff0706987e73e9f0457e884
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-29 22:42:15 +0530

    added

--- src/components/Header/navbar/Index.tsx
+++ src/components/Header/navbar/Index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
-
+import { menuSlide } from '../anim';
 
 const navItems = [
     {
