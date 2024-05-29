29edf9c44dc3808a35913847ad538d1060e4a521
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-29 22:59:05 +0530

    updated index

--- src/components/Header/navbar/link/Index.tsx
+++ src/components/Header/navbar/link/Index.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { slide, scale } from '../../anim';
 
-export default function Index({data, isActive, setSelectedIndicator}) {
-  
-    const { title, href, index} = data;
-  
-    return (
-      <motion.div className={styles.link} onMouseEnter={() => {setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
-        <motion.div variants={scale} animate={isActive ? "open" : "closed"} className={styles.indicator}></motion.div>
-        <Link href={href}>{title}</Link>
-      </motion.div>
-    )
-  }
+export default function Index({data, isActive, setSelectedIndicator}: {data: any, isActive: any, setSelectedIndicator: any}) {
+    
+        const { title, href, index} = data;
+    
+        return (
+            <motion.div className={styles.link} onMouseEnter={() => {setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
+                <motion.div variants={scale} animate={isActive ? "open" : "closed"} className={styles.indicator}></motion.div>
+                <Link href={href}>{title}</Link>
+            </motion.div>
+        )
+    }
