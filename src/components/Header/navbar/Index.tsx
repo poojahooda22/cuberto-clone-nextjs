b7602f821d464b3d2f5ac344d30d45fd34d1b8bc
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-29 22:40:19 +0530

    added

--- src/components/Header/navbar/Index.tsx
+++ src/components/Header/navbar/Index.tsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+const navItems = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Work",
+      href: "/work",
+    },
+    {
+      title: "About",
+      href: "/about",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ]
+
 function Nav() {
   return (
     <div className='w-[20vw] h-full'>
