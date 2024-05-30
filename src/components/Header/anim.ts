775d52c6854d71b5a98d84c0241c816c1e0c7c38
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-30 11:53:31 +0530

    added

--- src/components/Header/anim.ts
+++ src/components/Header/anim.ts
@@ -14,3 +14,21 @@ export const scale = {
     open: {scale: 1, transition: {duration: 0.3}},
     closed: {scale: 0, transition: {duration: 0.4}}
 }
+
+const transition = {duration: 1, ease: [0.76, 0, 0.24, 1]}
+
+export const opacity = {
+    initial: {
+        opacity: 0,
+    },
+    open: {
+        opacity: 1,
+        transition: {
+            duration: 0.35
+        }
+    },
+    closed: {
+        opacity: 0,
+        transition: {duration: 0.35}
+    }
+}
